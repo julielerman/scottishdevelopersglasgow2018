8fbe9c995b7876f02b28d40fe850851aa38e0897
julie <julielerman@gmail.com>
2018-01-24 21:18:04 -0500

    initial

--- domain/team.cs
+++ domain/team.cs
@@ -1,41 +1,64 @@
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using SharedKernel;
 
-public class Team {
-  public Team (string teamName, string nickname, string yearFounded, string homeStadium) {
-    TeamName = teamName;
-    Nickname = nickname;
-    YearFounded = yearFounded;
-    HomeStadium = homeStadium;
-    Id = Guid.NewGuid ();
-  }
-  public Guid Id { get; private set; }
-  public string TeamName { get; private set; }
-  public string Nickname { get; private set; }
-  public string YearFounded { get; private set; }
-  public string HomeStadium { get; private set; } //encapsulate
-  public List<Player> Players { get; private set; }
-  public void AddPlayer (string firstName, string lastname) {
-      var fullName = PersonFullName.Create (firstName, lastname);
+namespace Domain {
+  public class Team {
+    public Team (string teamName, string nickname, string yearFounded, string homeStadium) : this () {
+      TeamName = teamName;
+      Nickname = nickname;
+      YearFounded = yearFounded;
+      HomeStadium = homeStadium;
+      Id = Guid.NewGuid ();
+      //newly created team starts out with empty players
+      //team retrieved from database including players will have instantiated Players,
+      //whereas from db without including players, Players will be null
+      Players = new List<Player> ();
+    }
+    private Team () {
+      // Players=new List<Player>();
+    }
+    public Guid Id { get; private set; }
+    public string TeamName { get; private set; }
+    public string Nickname { get; private set; }
+    public string YearFounded { get; private set; }
+    public string HomeStadium { get; private set; } //encapsulate
+    public List<Player> Players { get; private set; }
+    public bool AddPlayer (string firstName, string lastname, out string response) {
+      if (Players == null) {
+        //this will need to be tested with integration test
+        response = "You must first retrieve this team's existing list of players";
+        return false;
+      }
+      var fullName = PersonFullName.Create (firstName, lastname).FullName;
       var foundPlayer = Players.Where (p => p.Name.Equals (fullName)).FirstOrDefault ();
-      if (foundPlayer != null) {
+      if (foundPlayer == null) {
         Players.Add (new Player (firstName, lastname));
-        }
+        response = "Player added to team";
+        return true;
+      } else {
+        response = "Duplicate player";
+        return false;
       }
+    }
 
-      public Owner Owner { get; private set; }
-      public UniformColors HomeColors { get; set; }
-      public UniformColors AwayColors { get; set; }
-      public void ChangeOwnership (Owner newOwner) {
-        {
-          if (Owner.Name != null) {
-            if (Owner.Name != newOwner.Name) {
-              Owner.PastTeams.Add (Owner.CurrentTeamId);
-            }
+    public Manager Manager { get; private set; }
+    public UniformColors HomeColors { get; private set; }
+    public void ChangeManagement (Manager newManager) {
+      {
+        if (Manager != null) {
+          if (Manager.Name != newManager.Name) {
+            Manager.PastTeams.Add(new ManagerTeamHistory(Id, newManager.Id));
           }
-
         }
+        Manager = newManager;
       }
+    }
+    public void SpecifyHomeUniformColors (Color shirt1, Color shirt2, Color shirt3, Color shorts1, Color shorts2, Color socks) {
+      HomeColors = new UniformColors (shirt1, shirt2, shirt3, shorts1, shorts2, socks);
+       }
+  
+  }
 }
--- test/UnitTest1.cs
+++ test/UnitTest1.cs
@@ -1,14 +1,68 @@
 using System;
+using System.Drawing;
+using System.Linq;
+using Domain;
 using Xunit;
 
-namespace test
-{
-    public class UnitTest1
-    {
+namespace test {
+    public class DomainClassTests {
+
+        private static Team CreateTeamAjax () {
+            return new Team ("AFC Ajax", "The Lancers", "1900", "Amsterdam Arena");
+        }
+
         [Fact]
-        public void Test1()
-        {
+        public void NewTeamGetsId () {
+            var team = CreateTeamAjax ();
+            Assert.NotEqual (Guid.Empty, team.Id);
+        }
+
+        [Fact]
+        public void TeamAllowsNewPlayer () {
+            var team = CreateTeamAjax ();
+            var playerWasAdded = team.AddPlayer ("André", "Onana", out string response);
+            Assert.Equal ("André Onana", team.Players.First().Name);
 
         }
+
+        [Fact]
+        public void TeamAllowsMultiplePlayers () {
+            var team = CreateTeamAjax ();
+            team.AddPlayer ("André", "Onana", out string response);
+            team.AddPlayer ("Matthijs", "de Ligt", out response);
+            Assert.Equal (2, team.Players.Count ());
+        }
+
+        [Fact]
+        public void TeamPreventsDuplicatePlayer () {
+            var team = CreateTeamAjax ();
+            team.AddPlayer ("André", "Onana", out string response);
+            team.AddPlayer ("André", "Onana", out response);
+            Assert.Single (team.Players);
+        }
+
+        [Fact]
+        public void TeamReturnsDuplicateMessageForDuplicatePlayer () {
+            var team = CreateTeamAjax ();
+           
+            team.AddPlayer ("André", "Onana", out string response);
+            team.AddPlayer ("André", "Onana", out response);
+            Assert.Equal ("Duplicate player", response);
+        }
+
+        [Fact]
+        public void CanChangeManager () {
+            var team = CreateTeamAjax ();
+            team.ChangeManagement (new Manager ("Erik", "ten Hag"));
+            Assert.Equal ("Erik ten Hag", team.Manager.Name);
+        }
+
+        [Fact]
+        public void CanSetHomeColors () {
+            var team = CreateTeamAjax ();
+            team.SpecifyHomeUniformColors (Color.White, Color.Red, Color.Empty, Color.White, Color.Empty, Color.White);
+            Assert.Equal (Color.White, team.HomeColors.ShirtPrimary);
+        }
+
     }
 }
